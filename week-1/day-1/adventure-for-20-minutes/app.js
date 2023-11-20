4a4c43109a993570751e4f01b8d0674abb9440b1
Denis Ignatenko <luuffyakka@yandex.ru>
2023-11-20 16:25:05 +0300

    R0-R1

--- week-1/day-1/adventure-for-20-minutes/app.js
+++ week-1/day-1/adventure-for-20-minutes/app.js
@@ -1,1 +1,23 @@
 // твой код
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
+
+app.get("/status", (request, response) => {
+  response.send("HTTP server is working!");
+});
+
+app.get("/users", (request, response) => {
+  response.json({ data: { age: 26 }, error: null });
+});
+
+app.get("/home", (request, response) => {
+  response.send("Home!");
+});
+
+module.exports = { app };
